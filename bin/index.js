700031c9cb2a31889c0f1d055d32180d5010ce3f
Falestio <falestioltmpt@gmail.com>
2022-08-12 19:10:43 +0700

    feat: latihan generator

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,7 @@ const chalk = require('chalk')
 
 const {getJoke} = require('./../lib/request')
 const {setup} = require('./../lib/setup')
+const {latihan} = require('./../lib/latihan')
 
 const args = process.argv.slice(2,3)[0]
 const secondArgs = process.argv.slice(3,4)[0]
@@ -14,6 +15,10 @@ if(args == 'joke'){
 
 if(args == 'setup'){
     setup()
+}
+
+if(args == 'latihan'){
+    latihan()
 }
 
 if(args == undefined){
